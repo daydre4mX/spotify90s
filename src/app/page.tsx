3a880cffe23271ad4d3f39126815dddace3c96f8
Ahmed-Honorbound <ahmedalicisse02@gmail.com>
2025-04-10 00:22:29 -0400

    revert to rid of draggable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Lyrics from "./components/lyrics/lyrics";
 import Vis from "./components/visualizer/visualizer";
-import albumart from "./components/albumart/albumart";  
+import Albumart from "./components/albumart/albumart";  
 export default function Home() {
   return (
     <div className="grid grid">
       <Lyrics />
       <Vis />
-      <albumart/>
+      <Albumart/>
     </div>
   );
 }
