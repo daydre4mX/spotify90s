8841dea20de2705df131c436c5d381d194204145
Ahmed-Honorbound <ahmedalicisse02@gmail.com>
2025-04-10 00:20:37 -0400

    Slight

--- src/app/components/lyrics/lyrics.tsx
+++ src/app/components/lyrics/lyrics.tsx
@@ -1,27 +1,20 @@
 'use client';
-import React, { useRef } from 'react';
-import Draggable from 'react-draggable';
+import React from 'react';
 import { ResizableBox } from 'react-resizable';
 import 'react-resizable/css/styles.css';
 
 export default function Lyrics() {
-  const nodeRef = useRef(null);
-
   return (
-    <Draggable className = "z-1" nodeRef={nodeRef}>
-      <div ref={nodeRef}>
-        <ResizableBox
-          width={200}
-          height={200}
-          minConstraints={[100, 100]}
-          maxConstraints={[500, 500]}
-          resizeHandles={['se']}
-        >
-          <div className='bg-default-gray w-full h-full rounded-xl border border-gray-400 z-1'>
-            Lyrics
-          </div>
-        </ResizableBox>
+    <ResizableBox
+      width={200}
+      height={200}
+      minConstraints={[100, 100]}
+      maxConstraints={[500, 500]}
+      resizeHandles={['se']}
+    >
+      <div className='bg-default-gray w-full h-full rounded-xl border border-gray-400'>
+        Lyrics
       </div>
-    </Draggable>
+    </ResizableBox>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Lyrics from "./components/lyrics/lyrics";
 import Vis from "./components/visualizer/visualizer";
-import Album from "./components/albumart/albumart";  
-import Equalizer from "./components/equalizer/equalizer";
+import albumart from "./components/albumart/albumart";  
 export default function Home() {
   return (
     <div className="grid grid">
       <Lyrics />
       <Vis />
-      <Album/>
-      <Equalizer />
+      <albumart/>
     </div>
   );
 }
