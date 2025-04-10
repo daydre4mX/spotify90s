c41b1267d435dfacc62e206d0f10adf4cdc2ba16
kad <myleskmiller@gmail.com>
2025-04-09 23:02:58 -0400

    sasageyo

---
+++ src/app/components/visualizer/visualizer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const visualizer = () => {
+  return (
+    <div>
+        <div className='bg-default-gray w-2xs h-50 rounded-xl border-1 border-gray-400'>
+        </div>
+    </div>
+  );
+}
+// This is a comment
+//lolxdedd
+export default visualizer;
