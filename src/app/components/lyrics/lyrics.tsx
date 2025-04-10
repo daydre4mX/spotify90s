613e4820550fe4f8213ecdc09486e44aeb7bdfae
Ahmed-Honorbound <ahmedalicisse02@gmail.com>
2025-04-09 22:58:57 -0400

    PATOOYE

--- src/app/components/lyrics/lyrics.tsx
+++ src/app/components/lyrics/lyrics.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function lyrics() {
   return (
-    <div className='bg-default-gray w-2xs h-50 rounded-xl border-1 border-gray-400'>
-        Lyrics
+    <div className='resize bg-default-gray w-2xs h-50 rounded-xl border-1 border-gray-400'>
+        <h1 className='m-4'>
+            Lyrics PATOOYE
+        </h1>
     </div>
   )
 }
