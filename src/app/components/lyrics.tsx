24111805844ae9ed64dd3b3a2bf03fb7e370baa3
Ahmed-Honorbound <ahmedalicisse02@gmail.com>
2025-04-09 22:14:34 -0400

    ade components file, and set up a starter div

---
+++ src/app/components/lyrics.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function lyrics() {
+  return (
+    <div className='bg-default-gray w-2xs h-50 rounded-xl border-1 border-gray-400'>
+        Lyrics
+    </div>
+  )
+}
