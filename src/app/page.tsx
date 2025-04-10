932885c12e7a10ab82e41d532313441733ee331b
kad <myleskmiller@gmail.com>
2025-04-10 14:06:39 -0400

    refactor: replace dynamic import of react-draggable with direct import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
+'use client'; 
+import React from "react";
 import Image from "next/image";
 import Lyrics from "./components/lyrics/lyrics";
 import Vis from "./components/visualizer/visualizer";
 import dynamic from 'next/dynamic';
 import Albumart from "./components/albumart/albumart";  
-<<<<<<< HEAD
-const Draggable = dynamic(() => import('react-draggable'), { ssr: false });
-=======
->>>>>>> c69eb38ff3aef5aa8e2e60f55e6edf3d4b2ae49e
+import Draggable from "react-draggable";
 
 export default function Home() {
   //priority is decided by the order of the divs, last item = highest
   //affecting z in div allows to change priority
   const nodeRef = React.useRef(null);
+
   return (
-    <div className="display-flexsudo npm install -g --force nodemon grid grid-cols-3">
+    <div className="display-flex grid grid-cols-3">
       <Draggable nodeRef={nodeRef} cancel=".react-resizable-handle">
         <div ref={nodeRef} className="z-2 bg-red-200">
           <Lyrics />
