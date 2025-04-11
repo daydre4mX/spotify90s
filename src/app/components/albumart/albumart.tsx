5eb55a9ffab280f3773c0a4192a7baf9e617ba31
kad <myleskmiller@gmail.com>
2025-04-10 23:21:58 -0400

    feat: implement toggle functionality for album art, runtime, and visualizer components

--- src/app/components/albumart/albumart.tsx
+++ src/app/components/albumart/albumart.tsx
@@ -1,8 +1,7 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import dynamic from 'next/dynamic';
 import 'react-resizable/css/styles.css';
-
 const ResizableBox = dynamic(
   () =>
     import('react-resizable').then((mod) => mod.ResizableBox),
@@ -10,26 +9,45 @@ const ResizableBox = dynamic(
 );
 
 export default function Albumart() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleAlbum = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div>
-        <ResizableBox
-          width={200}
-          height={200}
-          minConstraints={[150, 150]}
-          maxConstraints={[500, 500]}
-          resizeHandles={['se']}
-        >
-          <div className="bg-default-gray w-full h-full rounded-xl border-3 border-gray-400">
-          <p className='m-1 text-center font-mono text-xs border-b border-gray-400'>
-              JAMZ.MUSIC ART
-            </p>
-            <div className='flex overflow-hidden justify-center m-2'>
-                <img>
-                </img>
+  <div>
+    <button 
+      className="transform translate-x-10 translate-y-1"
+      onClick={toggleAlbum}
+    >
+      {isOpen ? 'Close Album' : 'Open Album'}
+    </button>
+    {isOpen && (
+        <div className="relative flex row-auto">
+          <ResizableBox
+            width={200}
+            height={200}
+            minConstraints={[150, 150]}
+            maxConstraints={[500, 500]}
+            resizeHandles={['se']}
+          >
+            <div className="bg-default-gray w-full h-full rounded-xl border-3 border-gray-400">
+              <p className='m-1 text-center font-mono text-xs border-b border-gray-400'>
+                JAMZ.MUSIC ART
+              </p>
+              <div className='flex overflow-hidden justify-center m-2'>
+                <img alt="Album Art" />
               </div>
-          </div>
-        </ResizableBox>
+              <button 
+                className="absolute top-0 right-2 z-1 p-1 flex row-auto"
+                onClick={toggleAlbum}
+              >
+                X
+              </button>
+            </div>
+          </ResizableBox>
+        </div>
+      )}
     </div>
   );
 }
--- src/app/components/visualizer/visualizer.tsx
+++ src/app/components/visualizer/visualizer.tsx
@@ -4,7 +4,7 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import dynamic from 'next/dynamic';
 import 'react-resizable/css/styles.css';
 import { useTexture } from '@react-three/drei';
-import * as THREE from "three"
+import * as THREE from 'three';
 
 const ResizableBox = dynamic(
   () =>
@@ -12,54 +12,35 @@ const ResizableBox = dynamic(
   { ssr: false }
 );
 
-function returnAudioAsImage(audioStream){
-
-  return audioStream
+function returnAudioAsImage(audioStream: any) {
+  return audioStream;
 }
 
-const Scene = ({vertex, fragment}: {vertex: string, fragment: string}) => {
-  const meshRef = useRef(null!);
+const Scene = ({ vertex, fragment }: { vertex: string; fragment: string }) => {
+  const meshRef = useRef<any>(null);
 
-  console.log(vertex)
-  
-  let noiseTexture = useTexture("noise2.png");
-  
+  let noiseTexture = useTexture('noise2.png');
+
   const width = 8;
   const height = 8;
-  
-  
+
   useFrame((state) => {
     let time = state.clock.getElapsedTime();
-    
-    meshRef.current.material.uniforms.iTime.value = time; 
+    meshRef.current.material.uniforms.iTime.value = time;
     meshRef.current.material.uniforms.iChannel0.value = returnAudioAsImage(null);
-
   });
-  
+
   const uniforms = useMemo(
     () => ({
-      iTime: {
-        type: "f",
-        value: 1.0,
-      },
-      iResolution: {
-        type: "v2",
-        value: new THREE.Vector2(width,height),
-      },
-      iChannel0: {
-        type: "t",
-        value: null,
-      },
-      iMouse: {
-        type: "v4",
-        value: new THREE.Vector4(0,0,0,0),
-      },
+      iTime: { type: 'f', value: 1.0 },
+      iResolution: { type: 'v2', value: new THREE.Vector2(width, height) },
+      iChannel0: { type: 't', value: null },
+      iMouse: { type: 'v4', value: new THREE.Vector4(0, 0, 0, 0) },
     }),
     []
   );
-  
 
-  return(
+  return (
     <mesh ref={meshRef}>
       <planeGeometry args={[width, height]} />
       <shaderMaterial
@@ -69,92 +50,92 @@ const Scene = ({vertex, fragment}: {vertex: string, fragment: string}) => {
         side={THREE.DoubleSide}
       />
     </mesh>
+  );
+};
 
+function Box(props: any) {
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
+  useFrame((state, delta) => {
+    ref.current.rotation.y += delta;
+    ref.current.rotation.x += delta;
+  });
 
-  )
-}
-
-function Box (props: any) {
-  // This reference gives us direct access to the THREE.Mesh object.
-  const ref = useRef<THREE.Mesh>(null!)
-
-  // Hold state for hovered and clicked events.
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-
-  // Subscribe this component to the render-loop and rotate the mesh every frame.
-  useFrame((state,delta) => (
-  
-    ref.current.rotation.y += delta,
-    ref.current.rotation.x += delta
-  ));
-
-  // Return the view.
-  // These are regular three.js elements expressed in JSX.
   return (
-    <mesh      
+    <mesh
       {...props}
       ref={ref}
       scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    > 
-      <boxGeometry args={[1, 1, 1]} />      
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange' } />    
+      onClick={(event) => setClicked(!clicked)}
+      onPointerOver={(event) => setHovered(true)}
+      onPointerOut={(event) => setHovered(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
-  )
+  );
 }
 
 export default function Visualizer() {
+  const [vertex, setVertex] = useState('');
+  const [fragment, setFragment] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [vertex, setVertex] = useState("");
-  const [fragment, setFragment] = useState("");
+  const toggleVisualizer = () => {
+    setIsOpen(!isOpen);
+  };
 
-  const fragShaderSelect = 4
+  const fragShaderSelect = 4;
 
   useEffect(() => {
-
-    fetch("/vertexShader.glsl").then((res) => res.text())
-    .then((text) => {
-      setVertex(text)
-    });
-    fetch(`/fragmentShader${fragShaderSelect}.glsl`).then((res) => res.text())
-    .then((text) => {
-      setFragment(text)
-    });
-
-    
+    fetch('/vertexShader.glsl')
+      .then((res) => res.text())
+      .then((text) => setVertex(text));
+    fetch(`/fragmentShader${fragShaderSelect}.glsl`)
+      .then((res) => res.text())
+      .then((text) => setFragment(text));
   }, []);
-  
+
   return (
     <div>
-        <ResizableBox
-          width={200}
-          height={200}
-          minConstraints={[150, 150]}
-          maxConstraints={[500, 500]}
-          resizeHandles={['se']}
+      {!isOpen && (
+        <button
+          className="transform translate-x-10 translate-y-1"
+          onClick={toggleVisualizer}
         >
-          <div className="bg-default-gray w-full h-full rounded-xl border-3 border-gray-400 overflow-hidden">
-            <p className='m-1 text-center font-mono text-xs border-b border-gray-400'>
+          Open Visualizer
+        </button>
+      )}
+      {isOpen && (
+        <div className="relative inline-block">
+          <ResizableBox
+            width={200}
+            height={200}
+            minConstraints={[150, 150]}
+            maxConstraints={[500, 500]}
+            resizeHandles={['se']}
+          >
+            <div className="bg-default-gray w-full h-full rounded-xl border-3 border-gray-400 overflow-hidden">
+              <p className="m-1 text-center font-mono text-xs border-b border-gray-400">
                 JAMZ.Visualizer
               </p>
-            <Canvas>
-              <Scene vertex={vertex} fragment={fragment} />
-              <color attach="background" args={['#fff']} />
-              {/* 
-              <ambientLight intensity={0.5} />      
-              <spotLight intensity= {1000} position={[10, 10, 10]} angle={0.15} penumbra={1} />      
-              <pointLight intensity= {1000} position={[-10, -10, -10]} />      
-              <Box position={[-1.2, 0, 0]} />     
-              <Box position={[1.2, 0, 0]} />  */}
-            </Canvas>
-          </div>
-
-
-        </ResizableBox>
+              <Canvas>
+                <Scene vertex={vertex} fragment={fragment} />
+                <color attach="background" args={['#fff']} />
+                {/* You can include additional scene components here */}
+              </Canvas>
+            </div>
+          </ResizableBox>
+          <button
+            className="absolute top-0 right-2 z-10 p-1 flex row-auto"
+            onClick={toggleVisualizer}
+          >
+            X
+          </button>
+        </div>
+      )}
     </div>
   );
 }
