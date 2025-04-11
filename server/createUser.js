dd086a9e3dca2bc84bb1165dd48a4621868a015a
Jonathan Bell <162631100+JonathanBell-Dev@users.noreply.github.com>
2025-04-10 22:08:01 -0400

    Updated createUser.js

--- server/createUser.js
+++ server/createUser.js
@@ -1,6 +1,6 @@
 // routes/createUser.js
 import express from 'express'
-import supabase from './supabaseClient.js'
+import supabase from '../supabaseClient.js'
 
 const router = express.Router()
 
@@ -10,8 +10,15 @@ router.post('/create-user', async (req, res) => {
   const { data, error } = await supabase
     .from('users')
     .insert([{ id, spotify_id, display_name, email, avatar_url }])
+    .select() 
 
-    if (error) return res.status(400).json({ error: error.message })
+  
+  if (error) {
+    console.error('Supabase Insert Error:', error)
+    return res.status(400).json({ error: error.message })
+  }
+
+  
   res.json({ user: data })
 })
 
